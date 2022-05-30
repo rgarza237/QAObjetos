3f2bb62d4c21f25684e473d8b7b2a4efe0087065
rgarza237 <roberto.garzah@udem.edu>
2022-05-30 15:53:23 -0500

    finnished

--- objects.js
+++ objects.js
@@ -1,19 +1,19 @@
 class Shape {
-    constructor(color, filled ) {
-        this.color = "azul"
-        this.filled = false
+    constructor(color="azul", filled=false ) {
+        this._color = color
+        this._filled = filled
     }
     getColor(){
-        return this.color
+        return this._color
     }
     setColor(newColor){
-        this.color = newColor
+        this._color = newColor
     }
-    isFilled(){
-        return this.filled
+    getFilled(){
+        return this._filled
     }
     setFilled(newFilled){
-        this.filled = newFilled
+        this._filled = newFilled
     }
     getArea(){
         return
@@ -22,7 +22,7 @@ class Shape {
         return
     }
     toString(){
-        return `Shape[color=${this.color}, filled=${this.filled}]`
+        return `Shape[color=${this._color}, filled=${this._filled}]`
     }
 }
 
@@ -37,10 +37,19 @@ class Circle extends Shape{
     setRadius(newRadius){
         this.radius = newRadius
     }
+    getPerimeter(){
+        super.getPerimeter()
+        return 2*(3.14 * this.radius)
+    }
+    getArea(){
+        super.getArea()
+        return 3.14 * (this.radius * this.radius)
+    }
     toString(){
         super.toString()
-        return `Circle[Shape[color=${this.color}, filled=${this.filled}], radius=${this.radius}]`
+        return `Circle[Shape[color=${this.getColor()}, filled=${this.getFilled()}], radius=${this.radius}, perimeter=${this.getPerimeter()}, area=${this.getArea()}]`
     }
+
 }
 
 class Rectangle extends Shape{
@@ -61,9 +70,17 @@ class Rectangle extends Shape{
     setLength(newLength){
         this.length = newLength
     }
+    getPerimeter(){
+        super.getPerimeter()
+        return (2*this.width)+(2*this.length)
+    }
+    getArea(){
+        super.getArea()
+        return this.width * this.length
+    }
     toString(){
         super.toString()
-        return `Rectangle[Shape[color=${this.color}, filled=${this.filled}], width=${this.width}, length=${this.length}]`
+        return `Rectangle[Shape[color=${this.getColor()}, filled=${this.getFilled()}], width=${this.width}, length=${this.length}, perimeter=${this.getPerimeter()}, area=${this.getArea()}]`
     }
 }
 
@@ -78,9 +95,49 @@ class Square extends Rectangle{
     setSide(newSide){
         this.side = newSide
     }
+    getPerimeter(){
+        super.getPerimeter()
+        return this.side*4
+    }
+    getArea(){
+        super.getArea()
+        return this.side * this.side 
+    }
     toString(){
         super.toString()
-        return `Square[Rectangle[Shape[color=${this.color}, filled=${this.filled}],width=${this.width}, length=${this.length}], side=${this.side}]`
+        return `Square[Rectangle[Shape[color=${this.getColor()}, filled=${this.getFilled()}],width=${this.width}, length=${this.length}], side=${this.side}, perimeter=${this.getPerimeter()}, area=${this.getArea()}]`
+    }
+}
+
+class Triangle extends Shape{
+    constructor(width, height){
+        super()
+        this.width = 1.0
+        this.height = 1.0
+    }
+    getWidth(){
+        return this.width 
+    }
+    setWidth(newWidth){
+        this.width = newWidth
+    }
+    getHeight(){
+        return this.height 
+    }
+    setLength(newHeight){
+        this.length = newHeight
+    }
+    getPerimeter(){
+        super.getPerimeter()
+        return (2*this.height) + this.width
+    }
+    getArea(){
+        super.getArea()
+        return (this.width * this.height)/2
+    }
+    toString(){
+        super.toString()
+        return `Triangle[Shape[color=${this.getColor()}, filled=${this.getFilled()}], width=${this.width}, length=${this.height}, perimeter=${this.getPerimeter()}, area=${this.getArea()}]`
     }
 }
 
@@ -92,6 +149,8 @@ const rectangle = new Rectangle()
 console.log(rectangle.toString())
 const square = new Square()
 console.log(square.toString())
+const triangle = new Triangle()
+console.log(triangle.toString())
 
 //console.log(newCircle.setRadius(2))
 //console.log(newCircle.getRadius())
